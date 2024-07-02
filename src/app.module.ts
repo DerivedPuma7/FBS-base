d192ffd1eee6f0362a0bf2044f75f9cbc93a1f54
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2024-07-02 18:08:22 -0300

    sqs queue dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PinoLogger } from '@/infra/gateways/logger/pino.logger';
+import { SqsQueueModule } from './infra/gateways/sqs/sqs.module';
 
 @Module({
-  imports: [],
+  imports: [SqsQueueModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    PinoLogger,
+    AppService
+  ],
 })
 export class AppModule {}
