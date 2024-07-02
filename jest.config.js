3ddb08416bdb63a83ec3e1c72a59b843b93957b5
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2024-07-02 16:47:25 -0300

    test: jest config

---
+++ jest.config.js
@@ -0,0 +1,24 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+*/
+
+/** @type {import('jest').Config} */
+const config = {
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: "coverage",
+  coverageProvider: "babel",
+  moduleNameMapper: {
+    '@/test/(.+)': '<rootDir>/test/$1',
+    '@/(.+)': '<rootDir>/src/$1',
+  },
+  roots: [
+    '<rootDir>/src',
+    '<rootDir>/test'
+  ],
+  transform: {
+    '\\.ts$': 'ts-jest'
+  }
+};
+
+module.exports = config;
